5bc1420aa0017d5f4faae8eb5e752b6a3cd22329
Rodrigo Tolledo <rodrigotolledo@gmail.com>
2013-12-10 09:49:24 -0200

    adding first route to home page and gitignore.

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+import static spark.Spark.*;
+import spark.*;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        get(new Route("/welcome") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Bem vindo!";
+            }
+        });
+
+    }
+}
