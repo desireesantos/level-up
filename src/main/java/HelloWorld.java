0236d49271888118c78213495ab661ba5738ebfe
Desiree Santos <desisant@gmail.com>
2013-12-12 21:40:45 -0200

    return index.html using personality struct

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,24 +1,40 @@
-import spark.ModelAndView;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import spark.Request;
 import spark.Response;
-import spark.template.freemarker.FreeMarkerRoute;
+import spark.Route;
+
+import java.io.IOException;
+import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+
 import static spark.Spark.get;
 
 public class HelloWorld {
 
     public static void main(String[] args) {
 
-        get(new FreeMarkerRoute("/index") {
-            @Override
-            public ModelAndView handle(Request request, Response response) {
-                Map<String, Object> attributes = new HashMap<>();
-                attributes.put("message", "Hello World!");
+        final Configuration setup = new Configuration();
+        setup.setClassForTemplateLoading(HelloWorld.class, "/");
 
-                return modelAndView(attributes, "index.ftl");
+        get(new Route("/index") {
+            @Override
+            public Object handle(Request request, Response response) {
+                StringWriter view = new StringWriter();
+                Map<String, Object> attributes = new HashMap<>();
+                attributes.put("name", "Hello Word");
+                try {
+                    Template template = setup.getTemplate("view/index.html");
+                    template.process(attributes, view);
+                } catch (TemplateException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return view;
             }
         });
-
     }
 }
