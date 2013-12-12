4948d2f08bcaaf7143f8bdf486587859d7b03e5c
Desiree Santos <desisant@gmail.com>
2013-12-12 20:57:40 -0200

    Update .gitignore - No allow to push .idea files

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,10 @@
-import static spark.Spark.get;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.template.freemarker.FreeMarkerRoute;
+import java.util.HashMap;
+import java.util.Map;
+import static spark.Spark.get;
 
 public class HelloWorld {
 
@@ -18,8 +16,6 @@ public class HelloWorld {
                 Map<String, Object> attributes = new HashMap<>();
                 attributes.put("message", "Hello World!");
 
-                // The hello.ftl file is located in directory:
-                // src/test/resources/spark/template/freemarker
                 return modelAndView(attributes, "index.ftl");
             }
         });
