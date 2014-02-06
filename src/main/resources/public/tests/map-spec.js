31d9d7f604c3ae4cca9c7dc3aa4c881175da7b35
Fernanda Martins <flmmartins@gmail.com>
2014-02-06 20:24:28 -0200

    Fernanda/Raquel/Desiree: Implements JsonFetcher with unit test.

--- src/main/resources/public/tests/map-spec.js
+++ src/main/resources/public/tests/map-spec.js
@@ -1,6 +1,20 @@
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+
+describe("JsonFetcher", function() {
+    it("fetch points from server ", function(){
+        spyOn($, "getJSON");
+        var callback = function (pointsJson) {
+           console.log(pointsJson);
+        };
+        var url = "/coordinates";
+        var jsonFetcher = pandas.createJsonFetcher(url);
+        jsonFetcher.fetch(callback);
+
+        expect($.getJSON).toHaveBeenCalledWith(url, callback);
+    });
+});
+
+describe("Map Loader", function() {
+    it("loads the map", function(){
     });
 });
 
