e2ecac3a8fcf50cd762ae6daf3334566dd1ed4ac
pamcdm <pmori@pmori.local>
2014-02-18 09:13:33 -0300

    Refactor javascript tests

--- src/main/resources/public/tests/map-spec.js
+++ src/main/resources/public/tests/map-spec.js
@@ -1,17 +1,3 @@
-describe("JsonFetcher", function() {
-    it("fetch points from server ", function(){
-        spyOn($, "getJSON");
-        var callback = function (pointsJson) {
-           console.log(pointsJson);
-        };
-        var url = "/coordinates";
-        var jsonFetcher = pandas.createJsonFetcher(url);
-        jsonFetcher.fetch(callback);
-
-        expect($.getJSON).toHaveBeenCalledWith(url, callback);
-    });
-});
-
 describe("Map Loader", function() {
     it("Define the map", function(){
         spyOn(google.maps,"Map");
