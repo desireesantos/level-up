467a248451d3506f2b7f2c77132516a24714103d
Pamela Canchanya <pmori@pmori.local>
2014-02-25 10:10:34 -0300

    LBD-41 Center map on Brazil , added jasmine tests

--- src/main/resources/public/js/map.js
+++ src/main/resources/public/js/map.js
@@ -1,19 +1,28 @@
 var pandas = pandas || {};
 
-pandas.initialize = function() {
-    geocoder = new google.maps.Geocoder();
-
+pandas.initialize = function(map, geocoder) {
     var mapCenter = new google.maps.LatLng(-12.0716330, -77.0566020);
 
     var mapOptions = {
-        zoom: 2,
+        zoom: 3,
         center: mapCenter
     }
 
     pandas.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
+    pandas.centerMap(pandas.map, "Brazil");
     pandas.getCoordinates();
-}
+};
+
+pandas.centerMap = function(map, country){
+    geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode( {'address' : country}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+        }
+    });
+};
+
 
 function createMarker(coordinate, description) {
     var marker = new google.maps.Marker ({
@@ -26,9 +35,10 @@ function createMarker(coordinate, description) {
 pandas.getCoordinates = function() {
     var jsonFetcher = pandas.createJsonFetcher("/coordinates");
     jsonFetcher.fetch(drawCoordinatesOnMap);
-}
+};
 
 function drawCoordinatesOnMap(coordinates) {
+
     for (var i = 0; i < coordinates.length; i++) {
         var coordinate = coordinates[i];
         var coordinateToShowOnMap = new google.maps.LatLng(coordinate.latitude, coordinate.longitude);
--- src/main/resources/public/tests/map-spec.js
+++ src/main/resources/public/tests/map-spec.js
@@ -1,5 +1,5 @@
 describe("Map Loader", function() {
-    it("Define the map", function(){
+    it("defines the map", function(){
         spyOn(google.maps,"Map");
         spyOn(pandas, "getCoordinates");
 
@@ -11,7 +11,34 @@ describe("Map Loader", function() {
         expect(google.maps.Map).toHaveBeenCalled();
         expect(pandas.getCoordinates).toHaveBeenCalled();
     });
+
+    // make sure call center map when initialize
+    // make sure center the map of brazil coordinates
+
+
+    it("center the map when initialize", function(){
+        spyOn(pandas, "centerMap");
+        spyOn(google.maps, "Map");
+
+        pandas.initialize();
+
+        expect(pandas.centerMap).toHaveBeenCalled();
+        expect(pandas.centerMap).toHaveBeenCalledWith(pandas.map, "Brazil");
+    });
+
+    it("center the map on Brazil", function(){
+        pandas.centerMap(pandas.map, "Brazil");
+
+        var latitudeBR = 11.897013135795008;
+        var longitudeBR = -51.92527999999999;
+
+        expect(pandas.map.getCenter().lng()).toBe(longitudeBR);
+        expect(pandas.map.getCenter().lat()).toBe(latitudeBR);
+    });
+
 });
+
+
 
 
 
